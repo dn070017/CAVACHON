fb93757efb0d29adf8d4f5cd2dfa628bb64bfa1c
dn070017 <dn070017@gmail.com>
2025-04-08 10:59:47 +0800

    remove deprecated files

--- cavachon/layers/modifiers/distributions_preset/__init__.py
+++
@@ -1,9 +0,0 @@
-from .independent_bernoulli_modifier import (
-    IndependentBernoulliModifier as IndependentBernoulliModifier,
-)
-from .independent_zero_inflated_negative_binomial_modifier import (
-    IndependentZeroInflatedNegativeBinomialModifier as IndependentZeroInflatedNegativeBinomialModifier,
-)
-from .multivariate_normal_diag_modifier import (
-    MultivariateNormalDiagModifier as MultivariateNormalDiagModifier,
-)
