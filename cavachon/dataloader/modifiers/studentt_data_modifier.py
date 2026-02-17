Student-t dist added to Vanilla + GMM KL + BugFix

cavachon/dataloader/modifiers/studentt_data_modifier.py
@@ -7,7 +7,7 @@ from cavachon.environment.constants import Constants
 from cavachon.layers.modifiers.to_dense import ToDense
 
 
-class StudentTDataModifier(tf.keras.Model):
+class StudenttDataModifier(tf.keras.Model):
     def __init__(self, modality_name: str):
         super().__init__()
         self.modality_key = f"{modality_name}_{Constants.TENSOR_NAME_X}"

cavachon/layers/parameterizers/studentt_parameterizer_layer.py
@@ -1,11 +1,11 @@
 import tensorflow as tf
 
 
-class StudentTParameterizerLayer(tf.keras.layers.Layer):
+class StudenttParameterizerLayer(tf.keras.layers.Layer):
     def __init__(
         self,
         event_dims: int,
-        name: str = "student_t_parameterizer_layer",
+        name: str = "studentt_parameterizer_layer",
     ):
         super().__init__(name=name)
         self.event_dims = event_dims

cavachon/modules/preprocessors/modifiers/studentt_modifier.py
@@ -6,7 +6,7 @@ from cavachon.environment.constants import Constants
 from cavachon.layers.modifiers.to_dense import ToDense
 
 
-class StudentTModifier(tf.keras.Model):
+class StudenttModifier(tf.keras.Model):
     """
     Modifier for Student-T modalities used during the model's preprocessing
     step. This ensures that the data is converted to a Dense tensor